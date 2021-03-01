a9a24858eb06f9c9a1b984788c9c557543963769
Jinwoong Kim <jwkim@Jinwoongs-MacBook-Pro.local>
2021-03-01 21:36:42 +0900

    ADD: log_generator

---
+++ log_generator/main.py
@@ -0,0 +1,11 @@
+import logging
+import datetime
+import time
+
+# TODO log rotation
+
+logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.INFO)
+
+while True:
+    logging.info(datetime.datetime.now())
+    time.sleep(60)
