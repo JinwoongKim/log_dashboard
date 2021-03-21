6bf2c8b7765f248d6238d0d226e595b4604e8a34
JinwoongKim <aragnom@gmail.com>
2021-03-21 14:29:21 +0900

    ADD: test script remove and run all containers

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,9 @@
+docker rm -f toy_dev
 docker rm -f toy1_api-service_1
 docker rm -f toy1_nginx_1
+
+bash ./log_generator/build_and_run.sh
 docker-compose build
 docker-compose up -d
-docker exec -it toy1_api-service_1 bash
 
 
